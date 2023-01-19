272211aa15d9f3beb376b012b028786e78751d1d
camila assis <cabukater@gmail.com>
2023-01-19 12:27:26 -0300

    Update teste.ts

--- teste.ts
+++ teste.ts
@@ -1,48 +1,55 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Overlay } from '@angular/cdk/overlay';
-import { MatDialog } from '@angular/material/dialog';
-import { ModalBannerComponent } from './modal-banner/modal-banner.component';
+Para testar o código acima usando Jest com cobertura de 95%, você precisaria escrever testes unitários para o componente AppComponent e para o componente ModalBannerComponent.
+
+Aqui está um exemplo de como os testes unitários para o AppComponent poderiam ser escritos usando Jest:
+
+Copy code
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Overlay, OverlayModule } from '@angular/cdk/overlay';
+import { ModalBannerComponent } from './modal-banner/modal-banner.component';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-  let dialogSpy: jasmine.SpyObj<MatDialog>;
-  let overlaySpy: jasmine.SpyObj<Overlay>;
+  let dialog: MatDialog;
+  let overlay: Overlay;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ MatDialogModule, OverlayModule ],
+      declarations: [ AppComponent, ModalBannerComponent ],
+      providers: [ MatDialog, Overlay ]
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
-    dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
-    overlaySpy = jasmine.createSpyObj('Overlay', ['scrollStrategy']);
-
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
-      providers: [
-        { provide: MatDialog, useValue: dialogSpy },
-        { provide: Overlay, useValue: overlaySpy },
-      ],
-    }).compileComponents();
-
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    dialog = TestBed.get(MatDialog);
+    overlay = TestBed.get(Overlay);
+    fixture.detectChanges();
   });
 
-  it('should open the modal when the button is clicked', () => {
-    fixture.detectChanges();
-    const button = fixture.nativeElement.querySelector('button');
-    button.dispatchEvent(new Event('click'));
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-    expect(dialogSpy.open).toHaveBeenCalledWith(
-      ModalBannerComponent,
-      {
-        width: '75%',
-        panelClass: 'modal-banner/modal-banner.component',
-        height: '80%',
-        disableClose: true,
-        hasBackdrop: true,
-        ariaLabel: 'Modal de notícias',
-        scrollStrategy: overlaySpy.scrollStrategy
-      }
-    );
+  it('should open modal on button click', () => {
+    spyOn(dialog, 'open').and.callThrough();
+    spyOn(overlay, 'scrollStrategy').and.callThrough();
+    const button = fixture.debugElement.nativeElement.querySelector('button');
+    button.click();
+    fixture.detectChanges();
+    expect(dialog.open).toHaveBeenCalledWith(ModalBannerComponent, {
+      width: '75%',
+      panelClass: 'modal-banner/modal-banner.component',
+      height: '80%',
+      disableClose: true,
+      hasBackdrop: true,
+      ariaLabel: 'Modal de notícias',
+      scrollStrategy: overlay.scrollStrategy
+    });
   });
 });
-
